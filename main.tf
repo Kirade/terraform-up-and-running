9e83f3d582434cba98b41fea41016bbe46e4016e
JaeyoungHeo <jay.jaeyoung@gmail.com>
2020-10-11 17:07:26 +0900

    ADD: auto scailing group

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8080
+  default     = 8080
 }
+
 
 resource "aws_instance" "example" {
   ami                    = "ami-044057cb1bc4ce527"
@@ -25,6 +27,7 @@ EOF
   }
 }
 
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -36,8 +39,52 @@ resource "aws_security_group" "instance" {
       "0.0.0.0/0",
     ]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
 
 output "public_ip" {
   value = aws_instance.example.public_ip
 }
+
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-044057cb1bc4ce527"
+  instance_type   = "t2.nano"
+  security_groups = [
+    aws_security_group.instance.id,
+  ]
+
+  user_data = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p ${var.server_port} &
+EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+data "aws_availability_zones" "all" {
+  state = "available"
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  availability_zones   = data.aws_availability_zones.all.names
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
