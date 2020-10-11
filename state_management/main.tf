329b13dfe050a6190efba023f86e9a8ea360febe
JaeyoungHeo <jay.jaeyoung@gmail.com>
2020-10-12 00:25:42 +0900

    ADD: DynamoDB for lock tfstate

--- state_management/main.tf
+++ state_management/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-up-and-running-example-123"
-    key = "terraform.tfstate"
-    region = "ap-northeast-2"
+    bucket  = "terraform-up-and-running-example-123"
+    key     = "terraform.tfstate"
+    region  = "ap-northeast-2"
     encrypt = true
   }
 }
@@ -25,6 +25,18 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 
-output "s3_bucket_arn" {
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-up-and-running-lock"
+  hash_key       = "LockID"
+  read_capacity  = 2
+  write_capacity = 2
 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
 }
