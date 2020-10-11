f3c191d9ee332360337e843b90df4558b2c1ad67
JaeyoungHeo <jay.jaeyoung@gmail.com>
2020-10-11 23:55:28 +0900

    CHG: bucket name

--- state_management/main.tf
+++ state_management/main.tf
@@ -1,10 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-example-123"
+    key = "terraform.tfstate"
+    region = "ap-northeast-2"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-2"
 }
 
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state"
+  bucket = "terraform-up-and-running-example-123"
 
   versioning {
     enabled = true
@@ -13,4 +22,9 @@ resource "aws_s3_bucket" "terraform_state" {
   lifecycle {
     prevent_destroy = true
   }
+}
+
+
+output "s3_bucket_arn" {
+
 }
